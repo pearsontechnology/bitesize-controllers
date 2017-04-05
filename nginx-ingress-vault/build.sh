4f809dcac06607a1e4955f8ae01d9ce208d53276
jdarling <jeremy.darling@gmail.com>
2017-04-05 16:21:20 -0500

    Add help screen to build script

--- nginx-ingress-vault/build.sh
+++ nginx-ingress-vault/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 DRY_RUN=false
 
 VERSION_SOURCE_FILE="controller.go"
@@ -16,6 +14,8 @@ MAJOR=false
 MINOR=false
 PATCH=false
 UPREV=false
+TAG_LATEST=false
+TAG_EXISTS=false
 
 VERSION_LINE=`grep -oEi "const[\t ]+version[\t ]*=[\t ]*\"[0-9]+\.[0-9]+\.[0-9]+\"" $VERSION_SOURCE_FILE`
 VERSION=`echo $VERSION_LINE | grep -oEi "[0-9]+\.[0-9]+\.[0-9]+"`
@@ -26,6 +26,20 @@ TAG=$VERSION
 
 Showhelp () {
   echo "build.sh <options>"
+  echo ""
+  echo "  Options"
+  echo "    -? or -h or --help - Show this screen"
+  echo "    -P or --publish - Publish the build to the docker registry"
+  echo "    -f or --force - Force push the build to the docker registry even if it already exists"
+  echo "    -t <tag> or --tag <tag> - What tag to use when pushing"
+  echo "    -l or --tag-latest - Publish to docker registry as latest"
+  echo "    -v <version> or --version <version> - Composite version number to deploy"
+  echo "    -M or --major - Increment Major version number, reset minor and patch"
+  echo "    -m or --minor - Increment Minor version number, reset patch"
+  echo "    -p or --patch - Increment Patch version number"
+  echo "    -s or --source-file <fileName> - Set the source file to work against, defaults to \"$VERSION_SOURCE_FILE\""
+  echo "    --no-build - Don't execute go build"
+  echo "    --dry-run - Perform a dry run, nothing will actually change, but all commands will be output"
   exit 0
 }
 
@@ -71,6 +85,9 @@ do
       ;;
     --dry-run)
       DRY_RUN=true
+      ;;
+    -l|--tag-latest)
+      TAG_LATEST=true
       ;;
     -\?|-h|--help)
       Showhelp
@@ -119,6 +136,18 @@ if [[ $PUBLISH ]]; then
       echo "Tag $TAG already exists in the registry!"
       exit 1
     fi
+  else # We want to force a push even if the tag exists
+    if [[ $VERSION == $TAG ]]; then
+      CHECK=`echo $PUBLISHED_VERSIONS | grep -o "$VERSION"`
+      if [[ "$CHECK" != "" ]]; then
+        TAG_EXISTS=true
+      fi
+    else
+      CHECK=`echo $PUBLISHED_TAGS | grep -o "$TAG"`
+      if [[ "$CHECK" != "" ]]; then
+        TAG_EXISTS=true
+      fi
+    fi
   fi
 fi
 
@@ -152,7 +181,14 @@ if [[ $PUBLISH == true ]]; then
     echo docker build -t $REPOSITORY:$TAG .
     echo docker push $REPOSITORY:$TAG
   else
+    if [[ $TAG_EXISTS == true ]]; then
+      docker rmi $REPOSITORY:$TAG
+    fi
     docker build -t $REPOSITORY:$TAG .
     docker push $REPOSITORY:$TAG
+    if [[ $TAG_LATEST == true ]]; then
+      docker rmi $REPOSITORY:latest
+      docker push $REPOSITORY:latest
+    fi
   fi
 fi
